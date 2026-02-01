942b95c322e57c4651f6b4dd89cbce44cea1193e
yogowbw <168393907+yogowbw@users.noreply.github.com>
2026-02-01 11:26:42 +0700

    Delete src directory

--- src/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getOverview, getSensor } = require('../controllers/dashboardController');
-
-const router = express.Router();
-
-router.get('/overview', getOverview);
-router.get('/sensor/:sensorId', getSensor);
-
-module.exports = { dashboardRouter: router };
